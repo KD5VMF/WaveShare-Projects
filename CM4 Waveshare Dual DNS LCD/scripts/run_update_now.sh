98d1af8182d03a9daefbbc3dccba647881b2ce2c
KD5VMF <54245381+KD5VMF@users.noreply.github.com>
2026-05-10 14:41:21 -0500

    Delete CM4 Waveshare Dual DNS LCD directory

--- CM4 Waveshare Dual DNS LCD/scripts/run_update_now.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-echo "This will run apt upgrades and reboot when finished."
-echo "Press Ctrl+C now to cancel."
-sleep 5
-systemctl start cm4-safe-nightly-update.service
